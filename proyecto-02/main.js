32e6b8fa3f10c3e399898b642539bc175f78d707
andres_arenas <osorioandre204@gmail.com>
2021-09-18 00:21:37 -0500

    Suma dinamica

--- proyecto-02/main.js
+++ proyecto-02/main.js
@@ -10,3 +10,20 @@ const microondas = (comida, milisegundos) => {
 }
 
 microondas("Pizza", 3000);
+
+// Suma Dinamica
+
+const numero1 = parseInt(prompt("Ingrese cualquier número"));
+const numero2 = parseInt(prompt("Ingrese otro número"));
+
+const suma = (numero1, numero2) => {
+    setTimeout(() => {
+        if(typeof numero1 === "number" && typeof numero2 === "number") {
+            return alert(`La suma de los números que acaba de ingresar es: ${numero1 + numero2}`);
+        } else {
+            alert("Ambos deben ser números")
+        }        
+    }, 2000)
+}
+
+suma(numero1, numero2);
